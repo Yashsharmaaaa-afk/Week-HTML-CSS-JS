cc6cb0d18eb6a7ce758d5fb018e3ce1645bfb93a
Yashsharmaaaa-afk <yashjsharma.128@gmail.com>
2025-06-09 13:18:57 +0530

    Add files via upload

---
+++ marks.js
@@ -0,0 +1,21 @@
+let marks = 36;
+if (marks > 35) {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+for(let i=0;i<10;i++){
+  console.log(i);
+}
+let obj {
+  name: "yash",
+  gender:"male",
+};
+console.log(`my name is $(obj.age)`)
+console.log(obj.age);
+
+for(let key in obj)
+{
+  console.log(obj[key]);
+}
